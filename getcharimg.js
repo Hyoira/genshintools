2169e735b46ebbbddede8fd8baa1fca705f07fbe
Hyoira <35867279+Hyoira@users.noreply.github.com>
2022-02-28 19:56:05 +0900

    Add files via upload

---
+++ getcharimg.js
@@ -0,0 +1,9 @@
+npm install genshin-db
+
+const genshindb = require('genshin-db');
+
+imgs = genshindb.characters('kokomi', {resultLanguage:'JP'}).images
+
+namelist = genshindb.characters('names', { matchCategories: true })
+
+console.log(imgs)
